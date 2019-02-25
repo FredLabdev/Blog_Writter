53967da2a38fa031ad8fb78133546f27fbfccc79
Frédéric LABOUREL <fredericlabourel@macbook-air-de-frederic.home>
2019-02-25 17:10:56 +0100

    Rectif accés sans login + Singleton sur Manager dbConnect()

--- model/Manager.php
+++ model/Manager.php
@@ -4,8 +4,12 @@ namespace FredLab\tp4_blog_ecrivain\Model;
 
 class Manager {
     
+    private $db; 
+    
     protected function dbConnect() {
-        $db = new \PDO('mysql:host=db756345555.db.1and1.com;dbname=db756345555;charset=utf8', 'dbo756345555', 'Forteroche&2019', array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
-        return $db;
+        if ($this->db == null) {
+            $this->db = new \PDO('mysql:host=db756345555.db.1and1.com;dbname=db756345555;charset=utf8', 'dbo756345555', 'Forteroche&2019', array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
+        };
+        return $this->db;
     }
 }
