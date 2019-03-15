702362741d6dcd452fc85ba784c56b2ced4d0d32
Frédéric LABOUREL <fredericlabourel@macbook-air-de-frederic.home>
2019-03-15 12:04:12 +0100

    Modif sur page login

--- view/frontend/loginView.php
+++ view/frontend/loginView.php
@@ -4,7 +4,7 @@ $template = 'frontend';
 ob_start(); 
 ?>
 
-<div class="loginView">
+<div class="loginView container">
     <div class="login">
         <h1 class="login-brand">
             <div class="col-xs-12">Pour voyager sur l'ensemble du site,</div>
@@ -13,6 +13,13 @@ ob_start();
         <h2 class="login-title">
             Billet déjà prit ?<br>Embarquez :
         </h2>
+        <div class="row justify-content-center align-items-center">
+            <?php if($login_error) { ?>
+            <span class="alert alert-danger col-xs-4">
+                <?= $login_error; ?>
+            </span>
+            <?php } ?>
+        </div>
         <form class="login-form" method="post" action="index.php?action=login">
             <p>
                 <label>Votre pseudo : <input type="text" name="pseudo_connect" placeholder="pseudo" /></label>
@@ -21,19 +28,22 @@ ob_start();
             </p>
             <button type="button submit" class="btn btn-outline-light btn-lg col-xs-12" name="login">Alaska</button>
         </form>
-        <div class="row col-xs-12 text-center">
-            <?php if($login_error) { ?>
-            <span class="alert alert-danger col-xs-4 offset-4">
-                <?= $login_error; ?>
-            </span>
-            <?php } ?>
+        <div class="row justify-content-center align-items-center">
+            <a href="#newMember" type="button submit" class="btn btn-outline-warning btn-lg" name="login-form">Nouveau membre ?</a>
         </div>
     </div>
 
-    <div class="newMember">
+    <div class="newMember" id="newMember">
         <h2 class="member-title">
             Un billet pour l'Alaska ?
         </h2>
+        <div class="row justify-content-center align-items-center">
+            <?php if($message_error) { ?>
+            <span class="alert alert-danger col-xs-4">
+                <?= $message_error; ?>
+            </span>
+            <?php } ?>
+        </div>
         <form class="login-form" method="post" action="index.php?action=newMember">
             <p>
                 <label>Votre nom : <input type="text" name="name" placeholder="last name" /></label>
@@ -46,13 +56,6 @@ ob_start();
             </p>
             <button type="button submit" class="btn btn-outline-light btn-lg col-xs-12" name="login">Check-In</button>
         </form>
-        <div class="row col-xs-12 text-center">
-            <?php if($message_error) { ?>
-            <span class="alert alert-danger col-xs-4 offset-4">
-                <?= $message_error; ?>
-            </span>
-            <?php } ?>
-        </div>
     </div>
 </div>
 
